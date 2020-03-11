69d68bbf7527196bc7b8b2db3fe1d01a15c46ff4
Hamlet <hamlet.villa@gmail.com>
2020-03-11 14:18:55 -0400

    check if string brackets valid

---
+++ Random/bracket_validator.py
@@ -0,0 +1,24 @@
+"""Write an efficient function that tells us whether or not an input
+string's openers and closers are properly nested."""
+
+# openers = ['(', '{', '[']
+# closers = [')', '}', ']']
+
+def bracket_validator_function(string):
+    bracket_dict = {'(':')', '{':'}', '[':']'}
+    openers = ['(', '{', '[']
+    closers = [')', '}', ']']
+    bracket_stack = []
+
+    for char in string:
+        if char in openers:
+            bracket_stack.append(char)
+        elif char in closers:
+            if not bracket_stack:
+                return False
+            else:
+                bracket_stack_top = bracket_stack.pop()
+                if not bracket_dict[bracket_stack_top] == char:
+                    return False
+
+    return bracket_stack == []
